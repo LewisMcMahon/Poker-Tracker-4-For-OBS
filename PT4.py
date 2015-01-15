db93a5215eb8ac46a3d1159de8d1ffbb426f257d
Lewis McMahon <lewis@lewismcmahon.co.uk>
2015-01-15 06:09:49 +0000

    Added live tournaments and fixed some issues with tournaments past midnight not appearing in the right "day" as specified by the time offset

--- PT4.py
+++ PT4.py
@@ -8,7 +8,7 @@ from pprint import pprint
 username        = "HeDares"
 currency        = "USD"
 currencyConvert = False
-countActive     = True
+countActive     = False
 ##use if you have sessons that go past midnight
 timeOffset      =  0
 
@@ -43,15 +43,21 @@ def gettourneyData(username,currency):
       tourney_summary.currency, 
       tourney_summary.val_curr_conv,
       tourney_results.val_finish,
-      tourney_results.id_tourney
+      tourney_results.id_tourney,
+      tourney_summary.cnt_players,
+      tourney_table_type.val_seats,
+      tourney_table_type.val_tables,
+      tourney_table_type.val_speed
       
     FROM 
       public.tourney_summary, 
       public.player, 
-      public.tourney_results
+      public.tourney_results,
+      public.tourney_table_type
     WHERE 
       tourney_summary.id_tourney = tourney_results.id_tourney AND
       tourney_results.id_player = player.id_player AND
+      tourney_summary.id_table_type = tourney_table_type.id_table_type AND
       player.player_name = '"""+str(username)+"""' AND 
       tourney_summary.currency = '"""+str(currency)+"""'
     ORDER BY
@@ -78,7 +84,11 @@ def gettourneyData(username,currency):
                     "currency"      : row[12],
                     "val_curr_conv" : row[13],
                     "val_finish"    : row[14],
-                    "id_tourney"    : row[15]}
+                    "id_tourney"    : row[15],
+                    "cnt_players"   : row[15],
+                    "val_seats"     : row[15],
+                    "val_tables"    : row[15],
+                    "val_speed"     : row[15]}
         tourneyData.append(tempDict)
         
     return tourneyData
@@ -87,7 +97,6 @@ def calcTournamentNetValue(tourneyRow):
     netWon = tourneyRow["amt_won"]
     netWon += (tourneyRow["amt_bounty"]*tourneyRow["cnt_bounty"])
     netWon -= (tourneyRow["amt_buyin"]+tourneyRow["amt_fee"]+(tourneyRow["amt_rebuy"]*tourneyRow["cnt_rebuy"])+(tourneyRow["amt_addon"]*tourneyRow["cnt_addon"]))
-    #print str(netWon)+" "+str(tourneyRow["date_end"])
     
     
     ##convert to local curency in pt4 if set
@@ -100,46 +109,83 @@ def getEpoch(inputDateTime):
     return (inputDateTime-datetime.datetime(1970,1,1)).total_seconds()
 
 def getStats(tourneyData):
-    stats = {"Live" :           {"Net Won" : 0, "tournaments" : []}, 
-            "Today" :           {"Net Won" : 0, "played" : 0, "hands" : 0}, 
-            "Yesterday" :       {"Net Won" : 0, "played" : 0, "hands" : 0}, 
-            "This Week" :       {"Net Won" : 0, "played" : 0, "hands" : 0}, 
-            "This Month" :      {"Net Won" : 0, "played" : 0, "hands" : 0}, 
-            "All Time":         {"Net Won" : 0, "played" : 0, "hands" : 0}
+    stats = {"Live" :           {"Net Won" : 0, "Tournaments" : []}, 
+            "Today" :           {"Net Won" : 0, "Time Played" : 0, "Hands" : 0}, 
+            "Yesterday" :       {"Net Won" : 0, "Time Played" : 0, "Hands" : 0}, 
+            "This Week" :       {"Net Won" : 0, "Time Played" : 0, "Hands" : 0}, 
+            "This Month" :      {"Net Won" : 0, "Time Played" : 0, "Hands" : 0}, 
+            "All Time":         {"Net Won" : 0, "Time Played" : 0, "Hands" : 0}
     }
     
     for tourney in tourneyData:
         tourneyNetWon = calcTournamentNetValue(tourney)
         
-        tournamentStartDateADJ = datetime.datetime.fromtimestamp(getEpoch(tourney["date_end"])-timeOffset)
+        tournamentStartDateADJ = datetime.datetime.fromtimestamp(getEpoch(tourney["date_start"])+timeOffset)
+        
+        secsSinceEnd = datetime.datetime.now()-tourney["date_end"]
+        secsSinceEnd = secsSinceEnd.total_seconds()
         
         #active
-        if tourney["val_finish"] == "" and tournamentStartDateADJ.date() == datetime.datetime.now().date: 
-            print "live"
-                   
-            if countActive == True and tourney["date_end"] == "":
-                tourney["date_end"] = datetime.datetime.now()
-            elif tourney["date_end"] == "":
-                tourney["date_end"] = datetime.datetime.now()
-                #tourneyNetWon = Decimal(0)
+        if secsSinceEnd < 60*4: 
+            try:
+                conn = psycopg2.connect("dbname='PT4 DB' user='postgres' host='localhost' password='postgres'")
+            except:
+                print "I am unable to connect to the database"
+            
+            cur = conn.cursor()
+    
+            cur.execute("""SELECT 
+                              player.id_player, 
+                              tourney_hand_player_statistics.amt_p_effective_stack, 
+                              tourney_blinds.amt_bb, 
+                              tourney_hand_player_statistics.id_hand
+                            FROM 
+                              public.player, 
+                              public.tourney_hand_player_statistics, 
+                              public.tourney_blinds
+                            WHERE 
+                              player.id_player = tourney_hand_player_statistics.id_player AND
+                              tourney_blinds.id_blinds = tourney_hand_player_statistics.id_blinds AND
+                              tourney_hand_player_statistics.id_tourney = """+str(tourney["id_tourney"])+""" AND 
+                              player.player_name = '"""+str(username)+"""';""")
+            
+            
+            tourneyRow = cur.fetchall()
+            
+            liveTourneyData = {"Buyin" : tourney["amt_buyin"]+tourney["amt_fee"],"Players" : tourney["cnt_players"],"Seats" : tourney["val_seats"], "Tables" : tourney["val_tables"], "Speed" : tourney["val_speed"], "BB" : tourneyRow[0][2], "Stack" : tourneyRow[0][1]}
         
+        
+            if countActive == False:
+                tourneyNetWon = 0
+            
+            stats["Live"]["Tournaments"].append(liveTourneyData)
+        
+        nowDateTime = datetime.datetime.now()
+        secondsSinceMidnight = (nowDateTime - nowDateTime.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()        
+        if secondsSinceMidnight-timeOffset <= 0:
+            offsetMidnight = datetime.datetime(nowDateTime.year,nowDateTime.month,nowDateTime.day,0,0,0)+ datetime.timedelta(0,timeOffset)
+        else:
+            offsetMidnight = datetime.datetime(nowDateTime.year,nowDateTime.month,nowDateTime.day,0,0,0)+ datetime.timedelta(1,timeOffset)
+            
         #tooday
-        if tournamentStartDateADJ.date() == datetime.datetime.now().date():
+        tourneyDelta = offsetMidnight - tourney["date_start"]
+        tourneyDelta = tourneyDelta.total_seconds()
+        if tourneyDelta < secondsInDay:
             stats["Today"]["Net Won"] = stats["Today"]["Net Won"]+tourneyNetWon
         
-        #Yesterday    
-        if tournamentStartDateADJ.date() == datetime.date.fromordinal(datetime.date.today().toordinal()-1):            
+        #Yesterday  
+        if tourneyDelta < secondsInDay*2 and tourneyDelta > secondsInDay:              
             stats["Yesterday"]["Net Won"] =  stats["Yesterday"]["Net Won"]+tourneyNetWon 
         
         #thisWeek
         weekStart = datetime.date.today() - datetime.timedelta(days = datetime.date.today().weekday())
         weekEnd = weekStart + datetime.timedelta(days = 6)
         
-        if tournamentStartDateADJ.date() <= weekEnd and tournamentStartDateADJ.date() >= weekStart:
+        if tourney["date_start"].date() <= weekEnd and tourney["date_start"].date() >= weekStart:
             stats["This Week"]["Net Won"] = stats["This Week"]["Net Won"]+tourneyNetWon
         
         #month
-        if tournamentStartDateADJ.month == datetime.date.today().month:
+        if tourney["date_start"].month == datetime.date.today().month:
             stats["This Month"]["Net Won"] = stats["This Month"]["Net Won"]+tourneyNetWon
                 
         #all time
@@ -150,8 +196,13 @@ def writeFiles(stats):
     for timePoint in stats:
         if not os.path.exists(timePoint):
             os.makedirs(timePoint)
-        if timePoint == "live":
-            pass    
+        if timePoint == "Live":
+            statFile = open(timePoint+"/Tournaments.txt", "wb")
+            for liveTournament in stats[timePoint]["Tournaments"]:
+                line = "Buy in: $"+str(liveTournament["Buyin"])+" BB:"+str(round(liveTournament["Stack"]/liveTournament["BB"],1))+"\n"                                
+                statFile.write(line)
+            statFile.close()
+            
         else:
             for stat in stats[timePoint]:
                 stats[timePoint][stat]
@@ -163,7 +214,7 @@ while True == True:
     writeFiles(getStats(gettourneyData(username,currency)))
     print "files Writen at "+str(datetime.datetime.time(datetime.datetime.now()))
     break
-    time.sleep(30)
+    time.sleep(5)
 
 
 
